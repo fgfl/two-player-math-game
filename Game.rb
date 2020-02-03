e0ad0c80a44807a55b20505fca0a0614413cb37b
Frederick <frederick.gfl@gmail.com>
2020-02-03 19:57:33 +0000

    Add Game class
    - players stored in array
    - current_player ins index for array
    - switch player moves index in array
    - setting current player is private to prevent cheating

--- Game.rb
+++ Game.rb
@@ -1,5 +1,24 @@
 class Game
-  def initialize
-    
+  attr_reader :current_player, :players
+
+  def initialize(p1 = {}, p2 = {})
+    @players = [p1, p2]
+    @current_player = 0
+  end
+
+  # GETTERS
+  def current_player_info
+    players[current_player]
+  end
+
+  # SETTERS
+
+  def switch_player
+    self.current_player = (current_player + 1) % self.players.size
+  end
+
+  private
+  def current_player= (index)
+    @current_player = index
   end
 end
--- main.rb
+++ main.rb
@@ -1,15 +1,21 @@
 require "./Player"
 require "./Game"
 
-
-game = Game.new
 p1 = Player.new("Joe")
 p2 = Player.new("Bob")
 
+game = Game.new(p1, p2)
 
-puts p1.lose_one_life
-puts p1.isDead?
-puts p1.lose_one_life
-puts p1.isDead?
-puts p1.lose_one_life
-puts p1.isDead?
+puts game.players
+puts "current player: #{game.current_player_info}"
+puts game.switch_player
+puts "current player: #{game.current_player_info}"
+puts game.switch_player
+puts "current player: #{game.current_player_info}"
+
+# puts p1.lose_one_life
+# puts p1.isDead?
+# puts p1.lose_one_life
+# puts p1.isDead?
+# puts p1.lose_one_life
+# puts p1.isDead?
